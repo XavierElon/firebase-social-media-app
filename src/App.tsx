e9c7be0f9092d24e87a7ffdaf8091472988ba851
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-10 15:41:11 -0400

    add tailwinds and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Home, NotFound } from './pages'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1 className="text-3xl font-bold underline">Hello World</h1> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom'
+const NotFound = () => {
+  console.log('not found')
+  return (
+    <div className="h-screen w-screen bg-zinc-900">
+      <div className="flex flex-col justify-center items-center h-full">
+        <div className="container">
+          <div className="section-inner primary-bordered">
+            <div className="corner-black bordered-primary top-left" />
+            <div className="corner-black bordered-primary bottom-right large" />
+            <h4 className="text-white text-center ">
+              On No, This Page Is Not Found!
+            </h4>
+
+            <div className="row w-row">
+              <div className="flex flex-col pb-6 gap-3">
+                <Link
+                  to="/"
+                  className="custom-button small white w-inline-block text-center"
+                >
+                  <div className="custom-button-hover primary" />
+                  <div className="corner-black bottom-right small" />
+                  <div className="corner-black top-left small" />
+                  <div className="custom-button-text text-sky-400">
+                    Return Home
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
