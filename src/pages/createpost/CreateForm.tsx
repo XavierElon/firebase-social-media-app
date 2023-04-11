a205fff912e1ea8bc0a45a6763f3ad12ca305fde
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-11 13:55:03 -0400

    post to firestore

--- src/pages/createpost/CreateForm.tsx
+++ src/pages/createpost/CreateForm.tsx
@@ -1,6 +1,9 @@
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
+import { addDoc, collection } from 'firebase/firestore'
+import { auth, db } from '../../config/firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 interface CreateFormData {
   title: string
@@ -8,6 +11,8 @@ interface CreateFormData {
 }
 
 const CreateForm = () => {
+  const [user] = useAuthState(auth)
+
   const schema = yup.object().shape({
     title: yup.string().required('You must add a title.'),
     description: yup.string().required('You must add a description.'),
@@ -21,8 +26,17 @@ const CreateForm = () => {
     resolver: yupResolver(schema),
   })
 
-  const onCreatePost = (data: CreateFormData) => {
+  const postsRef = collection(db, 'posts')
+
+  const onCreatePost = async (data: CreateFormData) => {
     console.log(data)
+    await addDoc(postsRef, {
+      // title: data.title,
+      // description: data.description,
+      ...data,
+      username: user?.displayName,
+      userId: user?.uid,
+    })
   }
 
   return (
