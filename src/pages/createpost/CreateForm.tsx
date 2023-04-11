e5221889b97f5cfebe79c4e2a1c958f89cbb39c0
Xavier Hollingsworth <xavierelon93@gmail.com>
2023-04-11 13:23:02 -0400

    form errors

--- src/pages/createpost/CreateForm.tsx
+++ src/pages/createpost/CreateForm.tsx
@@ -2,22 +2,48 @@ import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 
+interface CreateFormData {
+  title: string
+  description: string
+}
+
 const CreateForm = () => {
   const schema = yup.object().shape({
     title: yup.string().required('You must add a title.'),
     description: yup.string().required('You must add a description.'),
   })
 
-  const { register } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<CreateFormData>({
     resolver: yupResolver(schema),
   })
 
+  const onCreatePost = (data: CreateFormData) => {
+    console.log(data)
+  }
+
   return (
     <div>
-      <form>
-        <input placeholder="Title..." {...register('title')} />
-        <input placeholder="Description..." {...register('description')} />
-        <input type="submit" />
+      <form onSubmit={handleSubmit(onCreatePost)}>
+        <input
+          placeholder="Title..."
+          {...register('title')}
+          className="text-grey font-bold py-2 px-2 rounded"
+        />
+        <p className="text-red-500">{errors.title?.message}</p>
+        <input
+          placeholder="Description..."
+          {...register('description')}
+          className="text-grey font-bold py-2 px-2 rounded"
+        />
+        <p className="text-red-500">{errors.description?.message}</p>
+        <input
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        />
       </form>
     </div>
   )
--- src/pages/createpost/CreatePost.tsx
+++ src/pages/createpost/CreatePost.tsx
@@ -2,8 +2,8 @@ import CreateForm from './CreateForm'
 
 const CreatePost = () => {
   return (
-    <div>
-      <h1>Create Post</h1>
+    <div className="bg-zinc-900 h-screen w-screen block">
+      <h1 className="text-white">Create Post</h1>
       <CreateForm></CreateForm>
     </div>
   )
